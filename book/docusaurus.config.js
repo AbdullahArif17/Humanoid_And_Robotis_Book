539061139326882867150e5d3cbcc7ac104b6d69
Abdullah <abdullaharif893@gmail.com>
2025-12-18 23:16:36 +0500

    new

--- book/docusaurus.config.js
+++ book/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://humanoid-and-robotis-book.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages: https://<USERNAME>.github.io/<REPO>/
-  baseUrl: '/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'AbdullahArif17', // Usually your GitHub org/user name.
